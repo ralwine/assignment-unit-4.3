f3cabdcbec7d8d444f1c6b5964d2722c14dc1c6e
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-06-27 18:38:34 -0500

    just getting it here

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-
+console.log('!!!HERE!!!');
 
 
 
