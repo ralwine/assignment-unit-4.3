a926f6107dbad564f752bb6bb3b7feb64cb77171
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-02 07:29:06 -0500

    more testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,13 +4,17 @@ console.log('***** Cart Functions *****');
 
 // Create a global variable named basket and set it to an empty array.
 let basket = [];
+basket.length = 0;
 console.log(basket);
+//Why is there kale, and spinach in this empty basket?
+console.log(basket.length);
 // Create a function called addItem. It should:
 // - take an input parameter for a string *item*.
 // - add the new item to the global array *basket*.
 // - return *true* indicating the item was added.
-function addItem(item){
-    let addItem = basket.push(item);
+function addItem(item) {
+   let addItem = basket.push(item);
+    
     return true;
 }
 
@@ -19,11 +23,25 @@ console.log(basket);
 // Create a function called listItems. It should:
 // - loop over the items in the *basket* array.
 // - console.log each individual item on a new line.
+//function listItems(item) {
+//    while (i = 0, i > basket.length, i++) {
+    
+//    }
+//}
 
-
+//console.log(basket.push('troll doll'));
+//console.log(basket);
+//console.log(basket.push('slap bracelet'));
+//How did we end up with kale?!
+//console.log(basket);
 // Create a function called empty. It should:
 // - reset the *basket* to an empty array.
+//function empty(item) {
+// let empty = (basket.pop(item));
+//}
 
+//console.log(basket);
+//console.log(empty(basket));
 
 
 
